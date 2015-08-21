bb3e95fc4635e7f59f4051b90367e84db3e4c2e1
ecatkins <ecatkins@gmail.com>
2015-08-20 22:10:05 -0400

    Try and except on seed genres

--- project/spotify/seed.py
+++ project/spotify/seed.py
@@ -1,7 +1,3 @@
-#export SPOTIPY_CLIENT_ID='your-spotify-client-id'
-#export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
-#export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
-
 from spotify.models import Song, Artist, Genre
 from spotify.secret import *
 import pyen
@@ -15,43 +11,26 @@ def seed_gen():
     for song in all_songs:
         artist = Artist.objects.filter(name=song.artist)
         if len(artist) == 0:
-            print(song.artist)
-            new_artist = Artist(name=song.artist)
-            new_artist.save()
-            response = en.get('artist/profile', name=song.artist, bucket=['genre'])
-            genres = response['artist']['genres']
-            for genre in genres:
-                existing = Genre.objects.filter(name=genre['name'])
-                if len(existing) == 0:
-                    genre_object = Genre(name=genre['name'])
-                    genre_object.save()
-                    print(genre)
-                else:
-                    genre_object = existing[0]
-                new_artist.genres.add(genre_object)
-            new_artist.save()
+            try:
+                print(song.artist)
+                new_artist = Artist(name=song.artist)
+                new_artist.save()
+                response = en.get('artist/profile', name=song.artist, bucket=['genre'])
+                genres = response['artist']['genres']
+                for genre in genres:
+                    existing = Genre.objects.filter(name=genre['name'])
+                    if len(existing) == 0:
+                        genre_object = Genre(name=genre['name'])
+                        genre_object.save()
+                        print(genre)
+                    else:
+                        genre_object = existing[0]
+                    new_artist.genres.add(genre_object)
+                new_artist.save()
+            except:
+                continue
 
-#seed random users
-# My-username:
-# 2rkfKrh8R3muuHrkENu30P
-eddy_username = "11800860"
-def seed_random_users():
-    token = util.prompt_for_user_token(eddy_username)
-    sp = spotipy.Spotify(auth=token)
-    response = sp.featured_playlists()
 
-    while response:
-        playlists = response['playlists']
-        for i, item in enumerate(playlists['items']):
-            print(playlists['offset'] + i, item['name'])
-
-        if playlists['next']:
-            response = sp.next(playlists)
-        else:
-            response = None
-
-    new_search = sp.search(q="Totally Stress Free",type="playlist")
-    print(new_search[0])
 
 
 
