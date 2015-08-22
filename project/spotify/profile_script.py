1e27f0e5f6d2ba3745986975777dfbaf6612dd3c
ecatkins <ecatkins@gmail.com>
2015-08-22 12:41:33 -0400

    Profile model

--- project/spotify/profile_script.py
+++ project/spotify/profile_script.py
@@ -1,10 +1,10 @@
-from spotify.models import User, Profile
+from spotify.models import User, UserProfile
 
 def seedprofile():
     all_users = User.objects.all()
     for user in all_users:
-        profile = Profile.objects.filter(user=user)
+        profile = UserProfile.objects.filter(user=user)
         if len(profile) == 0:
-            new_profile = Profile(user=user, is_real=True)
+            new_profile = UserProfile(user=user, is_real=True)
             new_profile.save()
             
