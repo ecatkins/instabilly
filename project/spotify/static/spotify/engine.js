b96448cfe079bbfa67b8f58ffe9fec4822fc815a
ecatkins <ecatkins@gmail.com>
2015-08-24 18:52:52 -0400

    Playlist generation

--- project/spotify/static/spotify/engine.js
+++ project/spotify/static/spotify/engine.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+	
 	$("#number_songs").val(10)
+	
+
 	$("#neighbors").val(3)
 	$("#recency").val(10)
 	$("#rating").val(5)
@@ -8,24 +11,40 @@ $(document).ready(function() {
 
 
 	$("#generate_button").on('click', function() {
+	
 		var number_songs = $("#number_songs").val()
-		var neighbors = $("#neighbors").val()
-		var recency_effect = $("#recency").val()
-		var rating_effect = $("#rating").val()
-		var duplicate_artist = $("#duplicateartist").val()
-		var existing_playlist = $("#existingplaylist").val()
-
+		var neighbors = $('#neighbors').attr('data-slider')
+		var recency_effect =$('#recency').attr('data-slider')
+		var rating_effect = $('#rating').attr('data-slider')
+		var duplicate_artist = $("#duplicateartist").attr('data-slider')
+		var existing_playlist = $("#existingplaylist").attr('data-slider')
 		var post_data = {"number_songs":number_songs, "neighbors":neighbors, "recency_effect":recency_effect,"rating_effect":rating_effect,"duplicate_artist":duplicate_artist, "existing_playlist": existing_playlist}
+		console.log(post_data)
 
 		$.post('playlist', post_data, function(data) {
-			console.log("anything")
+			console.log(data['track_uris'])
+			var uris = data['track_uris']
+			string = ""
+			for (song in uris) {
+				string += uris[song] + ','
+			}
+			string = string.substring(0, string.length - 1);
+			console.log(string)
+			
+
+			$('#playlist_player').append('<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:' + string + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
+
+			
 
 		})
 
 	})
 
-
+	
 
 	$('.fa-question').popover()
+
+	$(document).foundation();
+	$(document).foundation('slider', 'reflow');
 })
 
