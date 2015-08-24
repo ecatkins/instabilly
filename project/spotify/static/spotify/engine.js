5742fb53ae1550d19ba7f4622558dea1156e4045
ecatkins <ecatkins@gmail.com>
2015-08-24 12:20:15 -0400

    Generating playlist html

--- project/spotify/static/spotify/engine.js
+++ project/spotify/static/spotify/engine.js
@@ -1,8 +1,21 @@
-$(document).foundation();
+$(document).ready(function() {
+	$("#number_songs").val(10)
+	$("#neighbors").val(3)
+	$("#recency").val(10)
+	$("#rating").val(5)
+	$("#duplicateartist").val(0)
+	$("#existingplaylist").val(0)
 
-$(document).foundation('reflow');
 
-$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
-  $(document).foundation('reflow');
-});
+	$("#generate_button").on('click', function() {
+		var number_songs = $("#number_songs").val()
+		var neighbors = $("#neighbors").val()
+		var recency_effect = $("#recency").val()
+		var rating_effect = $("#rating").val()
+		var duplicate_artist = $("#duplicateartist").val()
+		var existing_playlist = $("#existingplaylist").val()
+
+	
+	})
+})
 
